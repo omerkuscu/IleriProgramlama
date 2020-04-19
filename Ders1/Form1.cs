be7231b9c8a05b7007716eed1f83cf7c08f706d2
Ömer KUŞCU <okuscu@msn.com>
2020-04-19 16:29:10 +0300

    Merhaba Mesajı Ver :)

--- Ders1/Form1.cs
+++ Ders1/Form1.cs
@@ -16,5 +16,10 @@ namespace Ders1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Merhaba");
+        }
     }
 }
